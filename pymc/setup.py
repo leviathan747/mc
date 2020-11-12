cd9c80d8667cb529b61ad6719a894e4fa54fb0db
Levi Starrett <levi@roxsoftware.com>
2020-11-12 14:58:05 -0500

    testing exe bundling

--- pymc/setup.py
+++ pymc/setup.py
@@ -5,15 +5,24 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='pymc',
-      version='1.0.0',
-      description='Python executor for MC-3020',
-      author='Levi Starrett',
-      author_email='levi@roxsoftware.com',
-      url='https://github.com/xtuml/mc',
-      license='Apache 2.0',
-      keywords='xtuml bridgepoint',
-      packages=['mc3020'],
-      requires=['pyxtuml', 'pyrsl'],
-      include_package_data=True,
-      zip_safe=False)
+opts = dict(name='pymc',
+            version='1.0.0',
+            description='Python executor for MC-3020',
+            author='Levi Starrett',
+            author_email='levi@roxsoftware.com',
+            url='https://github.com/xtuml/mc',
+            license='Apache 2.0',
+            keywords='xtuml bridgepoint',
+            packages=['mc3020'],
+            requires=['pyxtuml', 'pyrsl'],
+            include_package_data=True,
+            zip_safe=False)
+
+try:
+    import py2exe
+    opts['console'] = ['mc3020/__main__.py']
+    opts['zipfile'] = None
+except:
+    pass
+
+setup(**opts)
